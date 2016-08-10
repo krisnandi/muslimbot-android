6d6b259ec6db5a3716380b5eef0f515b3fe34fa9
Irnanto Krisnandi <tridienth@gmail.com>
2016-08-10 21:49:06 +0700

    -add some vector assets -create main layout

--- app/src/main/java/com/krisnandi/prayertimes/MainActivity.java
+++ app/src/main/java/com/krisnandi/prayertimes/MainActivity.java
@@ -18,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.toolbar);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
     }
 
     @Override
